76f4ef858141b575edbe8e36bad668c8a99ea319
CleverMole <JpanagaGitHub128@gmail.com>
2024-03-17 23:01:33 +0800

    PHP tutorial 1

---
+++ testPhp.php
@@ -0,0 +1,18 @@
+<?php
+    $name = "James";
+    
+?>
+
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>James Still Learning</title>
+</head>
+<body>
+    <h2>PLEASE CLICK SUBSCRIBE BUTTON</h2>
+    <form action="" method="Post"><button type="submit" name="btn">subscribe</button></form>
+    
+</body>
+</html> -->
