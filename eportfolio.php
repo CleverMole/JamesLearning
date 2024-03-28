bbd1ca80cd86ac12bf6427a009dcb20aea645736
CleverMole <JpanagaGitHub128@gmail.com>
2024-03-28 15:39:17 +0800

    portfolio(trial)

---
+++ eportfolio.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/a7efacd833.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/eportfolio.css">
+    <title>James Panaga</title>
+</head>
+
+<body>
+    <div class="header-container">
+        <div class="header-title">
+            <h1>My Portfolio</h1>
+        </div>
+
+        <ul class="header-menu">
+            <li><a href="#home"><i class="fa-solid fa-house"></i>Home</a></li>
+            <li><a href="#"><i class="fa-solid fa-user"></i>About</a></li>
+            <li><a href="#"><i class="fa-solid fa-folder"></i>Works</a></li>
+            <li><a href="#"><i class="fa-solid fa-file"></i>Resume</a></li>
+            <li><a href="#"><i class="fa-solid fa-address-book"></i>Contact</a></li>
+        </ul>
+
+        <li><a href="#"><i class="fa-solid fa-bars"></i></a></li>
+
+    </div>
+
+    <div class="home-container">
+        <div class="myprofile" id="home">
+            <img src="img/myprofile.png" alt="">
+        </div>
+        <div class="home-info">
+            <h1>I am <span>James Panaga</span></h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio voluptas enim accusamus voluptatum deserunt aliquam sit ad laboriosam ipsum, totam error rerum, recusandae unde numquam quisquam, rem quia quis placeat?</p>
+        </div>
+
+
+    </div>
+
+    <div class="socmed">
+        <ul>
+            <li><a href=""><i class="fa-brands fa-facebook"></i></a></li>
+            <li><a href=""><i class="fa-brands fa-twitter"></i></a></li>
+            <li><a href=""><i class="fa-brands fa-square-instagram"></i></a></li>
+            <li><a href=""><i class="fa-brands fa-github"></i></a></li>
+        </ul>
+    </div>
+
+
+
+
+</body>
+
+</html>
